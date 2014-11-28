749c9316e8d77dde720251e5399ba6cee76b79f9
Freek de Bruijn <freekdb@gmail.com>
2014-11-28 21:13:00 +0100

    Added Java 8 query for year with most movies

--- src/main/java/com/github/freekdb/groovy_and_java_8/movies/MovieQueries.java
+++ src/main/java/com/github/freekdb/groovy_and_java_8/movies/MovieQueries.java
@@ -7,22 +7,67 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
- * Class with movie queries.
+ * Class with movie queries, comparing Java 7 versus Java 8 style.
  *
  * @author <a href="mailto:freekdb@gmail.com">Freek de Bruijn</a>
  */
 public class MovieQueries {
-    public static void main(final String[] args) {
+    /**
+     * Main method.
+     *
+     * @param arguments unused command-line arguments.
+     */
+    public static void main(final String[] arguments) {
         new MovieQueries().runQueries();
     }
 
+    /**
+     * Compare several queries on a movies list using Java 7 versus Java 8 style.
+     */
     private void runQueries() {
         final List<Movie> movies = readData("data/movies-mpaa.txt");
         System.out.println("movies.size(): " + movies.size());
+        System.out.println();
 
-        // In which year were the most movies released?
+        printYearWithMostMovies(movies);
+
+        // Most seen actor? Frank Welker
+
+
+        // Most seen actor in one year? Phil Hawn: 24 movies in 1999
+    }
+
+    /**
+     * Read the movies list from the input file.
+     *
+     * @param filePath the input file path.
+     * @return the movies list.
+     */
+    private List<Movie> readData(final String filePath) {
+        final List<Movie> movies = new ArrayList<>();
+        try {
+            final BufferedReader dataReader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = dataReader.readLine()) != null) {
+                movies.add(new Movie(line));
+            }
+            dataReader.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    /**
+     * Print the year in which the most movies were released (and the movie count), both in Java 7 and Java 8 style.
+     *
+     * @param movies the movie list.
+     */
+    private void printYearWithMostMovies(final List<Movie> movies) {
+        // Java 7 style.
         final Map<Integer, Integer> yearToMovieCount = new HashMap<>();
         for (final Movie movie : movies) {
             final int year = movie.getReleaseYear();
@@ -39,23 +84,16 @@ public class MovieQueries {
         }
         System.out.println("maximumMovieYear: " + maximumMovieYear);
         System.out.println("yearToMovieCount.get(maximumMovieYear): " + yearToMovieCount.get(maximumMovieYear));
+        System.out.println();
 
-        // Most seen actor? Frank Welker
-        // Most seen actor in one year? Phil Hawn: 24 movies in 1999
-    }
-
-    private List<Movie> readData(final String filePath) {
-        final List<Movie> movies = new ArrayList<>();
-        try {
-            final BufferedReader dataReader = new BufferedReader(new FileReader(filePath));
-            String line;
-            while ((line = dataReader.readLine()) != null) {
-                movies.add(new Movie(line));
-            }
-            dataReader.close();
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
-        return movies;
+        // Java 8 style.
+        final Map.Entry<Integer, Long> yearAndCountEntry = movies.stream()
+            .collect(Collectors.groupingBy(Movie::getReleaseYear, Collectors.counting()))
+            .entrySet().stream()
+            .max(Map.Entry.comparingByValue())
+            .get();
+        System.out.println("maximumMovieYear: " + yearAndCountEntry.getKey());
+        System.out.println("yearToMovieCount.get(maximumMovieYear): " + yearAndCountEntry.getValue());
+        System.out.println();
     }
 }
