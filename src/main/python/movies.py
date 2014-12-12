141801242c6c1d5d169ad91c8868294780b5814e
Freek de Bruijn <freekdb@gmail.com>
2014-12-13 00:16:06 +0100

    Even nicer Python version from Rob Hooft

--- src/main/python/movies.py
+++ src/main/python/movies.py
@@ -2,48 +2,37 @@
 # December 12th, 2014
 # Rob Hooft
 
-class MovieSink(object):
-    def __init__(self):
-        pass
-    def add(self, title, year, appendix, player):
-        pass
-    def report(self):
-        print "No-op"
-
-class MovieCounter(MovieSink):
+class MovieCounter(object):
+    doc = "Total number of movies %d"
     def __init__(self):
         self.n = 0
     def add(self, title, year, appendix, player):
         self.n += 1
     def report(self):
-        print "Total number of movies %d" % self.n
+        print self.doc % self.n
 
-class DistinctTitles(MovieSink):
+class DistinctTitles(MovieCounter):
+    doc = "Total number of distinct titles %d"
     def __init__(self):
         self.s = set()
     def add(self, title, year, appendix, player):
         self.s.add(title)
     def report(self):
-        print "Total number of distinct titles %d" % len(self.s)
+        print self.doc % len(self.s)
 
-class ClintFinder(MovieSink):
-    def __init__(self):
-        self.n = 0
+class ClintFinder(MovieCounter):
+    doc = "Clint Eastwood in %d"
     def add(self, title, year, appendix, player):
-            if 'Eastwood, Clint' in player:
-                    self.n += 1
-    def report(self):
-        print "Clint Eastwood in %d" % self.n
+        if 'Eastwood, Clint' in player:
+            self.n += 1
 
-class DistinctYears(MovieSink):
-    def __init__(self):
-        self.s = set()
+class DistinctYears(DistinctTitles):
+    doc = "Distinct years %d"
     def add(self, title, year, appendix, player):
         self.s.add(year)
-    def report(self):
-        print "Distinct years %d" % len(self.s)
 
-class MostBusyYear(MovieSink):
+class MostBusyYear(MovieCounter):
+    doc = "Busiest year %s with %d movies"
     def __init__(self):
         self.s = {}
     def add(self, title, year, appendix, player):
@@ -59,47 +48,37 @@ class MostBusyYear(MovieSink):
                 highest = v
             elif v == highest:
                 busy.append(k)
-        print "Busiest year %s with %d movies" % (busy, highest)
+        print self.doc % (busy, highest)
 
-class ProlificActor(MovieSink):
-    def __init__(self):
-        self.s = {}
+class YearsWithoutMovies(MostBusyYear):
+    doc = "Years without movies %s"
+    def report(self):
+        yrs = sorted(self.s.keys())
+        nomovies = []
+        for y in range(yrs[0], yrs[-1]+1):
+            if not self.s.has_key(y):
+                nomovies.append(y)
+        print self.doc % nomovies
+
+class ProlificActor(MostBusyYear):
+    doc = "Most prolific actor %s with %d movies"
     def add(self, title, year, appendix, player):
         for p in player:
             if self.s.has_key(p):
                 self.s[p] += 1
             else:
                 self.s[p] = 1
-    def report(self):
-        highest = 0
-        for k,v in self.s.items():
-            if v > highest:
-                prol = [k]
-                highest = v
-            elif v == highest:
-                prol.append(k)
-        print "Most Prolific Actor %s with %d movies" % (prol, highest)
 
-class ProlificActorYear(MovieSink):
-    def __init__(self):
-        self.s = {}
+class ProlificActorYear(MostBusyYear):
+    doc = "Most prolific actor in one year %s with %d movies"
     def add(self, title, year, appendix, player):
         for p in player:
             if self.s.has_key((p,year)):
                 self.s[(p,year)] += 1
             else:
                 self.s[(p,year)] = 1
-    def report(self):
-        highest = 0
-        for k,v in self.s.items():
-            if v > highest:
-                prol = [k]
-                highest = v
-            elif v == highest:
-                prol.append(k)
-        print "Most Prolific Actor in one year %s with %d movies" % (prol, highest)
 
-class Fork(MovieSink):
+class Fork(MovieCounter):
     def __init__(self):
         self.inferior = []
     def newsink(self, sink):
@@ -119,8 +98,8 @@ class FileSource(object):
             title, players = line.split('/', 1)
             title, year = title.split('(', 1)
             assert year[-1]==')', year
-            year = year[:4]
             appendix = year[5:]
+            year = int(year[:4])
             player = players.strip().split('/')
             self.sink.add(title, year, appendix, player)
 
@@ -128,7 +107,8 @@ f = Fork()
 f.newsink(MovieCounter())
 f.newsink(DistinctTitles())
 f.newsink(ClintFinder())
-f.newsink(DistinctYears())
+#f.newsink(DistinctYears())
+f.newsink(YearsWithoutMovies())
 f.newsink(ProlificActor())
 f.newsink(ProlificActorYear())
 f.newsink(MostBusyYear())
