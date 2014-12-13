7368674e6e32cc0188783c7cb6aa149a983cfbe3
david <david@allthingsdigital.nl>
2014-12-13 12:39:42 +0100

    Add most active actor in a year.

--- david/src/main/java/nl/allthingsdigital/moviedb/Movies.java
+++ david/src/main/java/nl/allthingsdigital/moviedb/Movies.java
@@ -41,9 +41,9 @@ public class Movies {
     private void run(String file) throws IOException {
         Stream<String> lines = Files.lines(Paths.get(file));
 
-        ActorConsumer actor = new ActorConsumer();
-        MovieConsumer titles = new MovieConsumer();
-        YearConsumer year = new YearConsumer();
+        ActorConsumer actor = new ActorConsumer(200_000);
+        MovieConsumer titles = new MovieConsumer(20_000);
+        YearConsumer year = new YearConsumer(100);
 
         lines.peek(titles).peek(year).forEach(actor);
 
@@ -88,8 +88,10 @@ public class Movies {
     private abstract class AbstractMovieDBConsumer implements Consumer<String> {
         abstract String report();
 
-        protected String getTitle(String line) {
-            return stripYear(line.split("/")[0]);
+        protected Pair<Integer,String> getTitle(String line) {
+            Integer year = getYear(line);
+            String title = stripYear(line.split("/")[0]);
+            return new Pair<>(year, title);
         }
 
         protected String stripYear(String line) {
@@ -119,19 +121,37 @@ public class Movies {
 
     private class ActorConsumer extends AbstractMovieDBConsumer {
 
-        Map<String,List<String>> actors = new ConcurrentHashMap<>();
+        Map<String,List<String>> actors;
+        Map<String,Map<Integer,Integer>> actorMoviesPerYear;
+
+        private ActorConsumer(int expectedSize) {
+            this.actorMoviesPerYear = new ConcurrentHashMap<>(expectedSize);
+            this.actors = new ConcurrentHashMap<>(expectedSize);
+        }
 
         @Override
         public void accept(String t) {
             int start = t.indexOf('/');
-            List<String> asList = Arrays.asList(t.substring(start + 1).split("/"));
-            String movie = getTitle(t);
-            asList.stream().forEach(A -> {
+            List<String> actorList = Arrays.asList(t.substring(start + 1).split("/"));
+            Pair<Integer,String> yearAndTitle = getTitle(t);
+            String movie = yearAndTitle.getSecond();
+            Integer year = yearAndTitle.getFirst();
+            actorList.stream().forEach(A -> {
                 actors.merge(A, getMovieList(movie),
                     (L1, L2) -> { L1.addAll(L2); return L1; } );
+                actorMoviesPerYear.merge(A, getMovieMap(year),
+                        (M1, M2) -> {
+                            M1.put(year, M1.getOrDefault(year, 0) + M2.getOrDefault(year, 0));
+                            return M1;
+                        });
             });
         }
 
+        private Map<Integer,Integer> getMovieMap(Integer year) {
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(year, 1);
+            return map;
+        }
         private List<String> getMovieList(String movie) {
             List<String> movies = new ArrayList<>();
             movies.add(movie);
@@ -153,6 +173,21 @@ public class Movies {
             return pair;
         }
 
+        public Pair<String,Integer> getActorWithMostMoviesInYear() {
+            Pair<String,Integer> pair = new Pair<>("", 0);
+            Pair<String,Integer> mostMovies = new Pair<>("", 0);
+            actorMoviesPerYear.forEach((actor, moviesPerYear) -> {
+                moviesPerYear.forEach((year, movies) -> {
+                    if (movies > mostMovies.getSecond()) {
+                        mostMovies.setSecond(movies);
+                        pair.setFirst(actor);
+                        pair.setSecond(year);
+                    }
+                });
+            });
+            return pair;
+        }
+
         @Override
         public String report() {
             StringBuilder report = new StringBuilder();
@@ -160,6 +195,12 @@ public class Movies {
             Pair<String, Integer> pair = getActorWithMostMovies();
             report.append(String.format("The actor with most movies is %s with %d movies.\n",
                     pair.getFirst(), pair.getSecond()));
+            pair = getActorWithMostMoviesInYear();
+            String actor = pair.getFirst();
+            Integer year = pair.getSecond();
+            Integer movies = actorMoviesPerYear.get(actor).get(year);
+            report.append(String.format("The most active actor in a year was %s with %d movies in %d",
+                    actor, movies, year));
             return report.toString();
         }
 
@@ -167,13 +208,18 @@ public class Movies {
 
     private class MovieConsumer extends AbstractMovieDBConsumer {
 
-        int count = 0;
-        Set<String> unique = new HashSet<>();
+        int count;
+        Set<String> unique;
+
+        private MovieConsumer(int expectedSize) {
+            this.count = 0;
+            this.unique = new HashSet<>(expectedSize);
+        }
 
         @Override
         public void accept(String t) {
             count++;
-            unique.add(getTitle(t));
+            unique.add(getTitle(t).getSecond());
         }
 
         @Override
@@ -183,7 +229,11 @@ public class Movies {
     }
 
     private class YearConsumer extends AbstractMovieDBConsumer {
-        Map<Integer,Integer> map = new ConcurrentHashMap<>();
+        Map<Integer,Integer> map;
+
+        private YearConsumer(int expectedSize) {
+            this.map = new ConcurrentHashMap<>(expectedSize);
+        }
 
         @Override
         public void accept(String t) {
